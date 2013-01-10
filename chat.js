ac0339b1075b46de60b53642f902e6b1dee98c71
James Pickard <james.pickard+github@gmail.com>
2013-01-09 22:53:41 -0500

    Fixed broadcasting to a room.

--- chat.js
+++ chat.js
@@ -8,23 +8,40 @@ module.exports = function (server, sessionStore, cookieParser) {
 		
 	sessionSockets.on('connection', function (err, socket, session) {
 		if (err) {
-			// TODO: Keep an eye on this issue: https://github.com/functioncallback/session.socket.io/issues/11
-			console.log(err);
-			throw new Error(err.error);
+			throw err;
 		}
 		
 		socket.on('subscribe', function(data) {
 			console.log(session.username + ' joined ' + data.room);
-			io.sockets.in(data.room).emit('message', {time: Date.now(), username: 'admin', message: session.username + ' has joined ' + data.room + '.'});
+			
+			//io.sockets.in(data.room).emit('message', {time: Date.now(), username: 'admin', message: session.username + ' has joined ' + data.room + '.'});
 			socket.join(data.room);
+			
+			socket.broadcast.to(data.room).emit('message', {time: Date.now(), username: 'admin', message: session.username + ' has joined ' + data.room + '.'});
 			socket.emit('message', {time: Date.now(), username: 'admin', message: 'You have joined ' + data.room + '.'})
+			
+			// TODO: Figure out how we can broadcast to a room.
 		});
 		
 		socket.on('unsubscribe', function(data) { socket.leave(data.room); })
 		
 		socket.on('message', function (data) {
+			
+			if (data.roomName === undefined) {
+				console.log(session.username + ' sent a message but did not specify a roomName so it is being discarded.');
+				console.log(data);
+				return;
+			}
+			
+			if (data.message === undefined) {
+				console.log(session.username + ' sent a message but did not specify a message so it is being discarded.');
+				console.log(data);
+				return;
+			}
+			
 			console.log(session.username + ' sent a message:' + data.message);
 			
+			//socket.broadcast.emit('message'
 			socket.broadcast.emit('message', {
 				time: Date.now(),
 				username: session.username,
