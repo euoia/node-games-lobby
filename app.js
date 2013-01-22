08236f611d0c347b2ad527f5b431e769b79eb104
James Pickard <james.pickard+github@gmail.com>
2013-01-21 21:50:56 -0500

    adding .gitignore

--- app.js
+++ app.js
@@ -1,31 +1,30 @@
-
 /**
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes')
-	, login = require('./routes/login')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path')
-	, lessMiddleware = require('less-middleware')
-	, RedisStore = require('connect-redis')(express);
+var express = require('express'),
+	routes = require('./routes'),
+	login = require('./routes/login'),
+	user = require('./routes/user'),
+	http = require('http'),
+	path = require('path'),
+	lessMiddleware = require('less-middleware'),
+	RedisStore = require('connect-redis')(express);
 
 var secret = "put me in a config file";
 var app = express();
 var cookieParser = express.cookieParser(secret);
 var sessionStore = new RedisStore;
 
-app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-	
+app.configure(function() {
+	app.set('port', process.env.PORT || 3000);
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'ejs');
+	app.use(express.favicon());
+	app.use(express.logger('dev'));
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+
 	// Sessions won't work unless you have these 3 in this order: cookieParser,
 	// session router.
 	app.use(cookieParser);
@@ -33,26 +32,26 @@ app.configure(function(){
 		secret: secret,
 		store: sessionStore
 	}));
-  app.use(app.router);
-	
+	app.use(app.router);
+
 	app.use(lessMiddleware({
 		src: __dirname + '/../public',
 		compress: true
 	}));
-	
-  app.use(express.static(path.join(__dirname, 'public')));
+
+	app.use(express.static(path.join(__dirname, 'public')));
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler());
+app.configure('development', function() {
+	app.use(express.errorHandler());
 });
 
 app.get('/', routes.index);
 app.get('/users', user.list);
 app.post('/login/doLogin', login.doLogin);
 
-var server = http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
+var server = http.createServer(app).listen(app.get('port'), function() {
+	console.log("Express server listening on port " + app.get('port'));
 });
 
 var chat = require('./chat')(server, sessionStore, cookieParser);
