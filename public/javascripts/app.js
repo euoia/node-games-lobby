78a9ba0583520d292f7572f82f75610d1572e6c3
James Pickard <james.pickard+github@gmail.com>
2013-10-30 16:37:46 +0000

    reindent

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,44 +1,44 @@
 requirejs.config({
-	shim: {
-		'jquery': {
-			exports: '$'
-		},
-		'underscore': {
-			exports: '_'
-		}
-	}
+  shim: {
+    'jquery': {
+      exports: '$'
+    },
+    'underscore': {
+      exports: '_'
+    }
+  }
 });
 
 define([
-		'jquery',
-		'chat',
-		'game_server',
-		'login'
-	], function($, Chat, GameServer, Login) {
+    'jquery',
+    'chat',
+    'game_server',
+    'login'
+  ], function($, Chat, GameServer, Login) {
 
-		var login;
+    var login;
 
-		$(document).ready(function() {
-			var chat = new Chat({
-				userListDiv: '#left-sidebar',
-				messagesUl: '#chat-room .chat-box ul',
-				messageScroll: '#content-body',
-				messageEntryForm: '#message-entry-form',
-				messageEntry: '#message-entry'
-			});
+    $(document).ready(function() {
+      var chat = new Chat({
+        userListDiv: '#left-sidebar',
+        messagesUl: '#chat-room .chat-box ul',
+        messageScroll: '#content-body',
+        messageEntryForm: '#message-entry-form',
+        messageEntry: '#message-entry'
+      });
 
-			var gameServer = new GameServer (chat);
+      var gameServer = new GameServer (chat);
 
-			// TODO: Move all chat logic out of Login and provide callback for loginSuccess.
-			login = new Login(
-				chat,
-				{
-					usernameInput: '#usernameInput',
-					loginForm: '#login',
-					logout: '#logout'
-				}
-			);
-		});
+      // TODO: Move all chat logic out of Login and provide callback for loginSuccess.
+      login = new Login(
+        chat,
+        {
+          usernameInput: '#usernameInput',
+          loginForm: '#login',
+          logout: '#logout'
+        }
+      );
+    });
 
-		return login;
+    return login;
 });
