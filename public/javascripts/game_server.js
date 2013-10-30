458b5cd463b6bd643abca0aa1fd3cc5f0550e7cc
James Pickard <james.pickard+github@gmail.com>
2013-10-30 16:46:40 +0000

    rename chat to command center

--- public/javascripts/game_server.js
+++ public/javascripts/game_server.js
@@ -1,19 +1,19 @@
 // TODO: The number of instances of emitting a simply message indicates that we
-// could possibly do with a chat.addSimpleCommand function.
+// could possibly do with a commandCenter.addSimpleCommand function.
 //
 define([], function() {
-  // Pass in a instance of Chat (to be renamed Console - or something).
-  function GameServer (chat) {
+  // Pass in a instance of the command center.
+  function GameServer (commandCenter) {
     this.chat = chat;
 
     // Commands.
-    chat.addCommand('listGames', function() { chat.emit('listGames'); });
-    chat.addCommand('listMatches', function() { chat.emit('listMatches'); });
-    chat.addCommand('createMatch', this.createMatch.bind(this));
-    chat.addCommand('joinMatch', this.joinMatch.bind(this));
+    commandCenter.addCommand('listGames', function() { commandCenter.emit('listGames'); });
+    commandCenter.addCommand('listMatches', function() { commandCenter.emit('listMatches'); });
+    commandCenter.addCommand('createMatch', this.createMatch.bind(this));
+    commandCenter.addCommand('joinMatch', this.joinMatch.bind(this));
 
     // Listener events.
-    chat.addListener('launchMatch', this.launchGame.bind(this));
+    commandCenter.addListener('launchMatch', this.launchGame.bind(this));
   }
 
   // ----------------------
@@ -21,22 +21,22 @@ define([], function() {
   // ----------------------
   GameServer.prototype.createMatch = function(gameID) {
     if (gameID === undefined) {
-      this.chat.addNotification(Date.now(), 'Must specify a game name.');
+      this.commandCenter.addNotification(Date.now(), 'Must specify a game name.');
       return;
     }
 
-    this.chat.emit('createMatch', {
+    this.commandCenter.emit('createMatch', {
       gameID: gameID
     });
   };
 
   GameServer.prototype.joinMatch = function(gameID) {
     if (gameID === undefined) {
-      this.chat.addNotification(Date.now(), 'Must specify a game name.');
+      this.commandCenter.addNotification(Date.now(), 'Must specify a game name.');
       return;
     }
 
-    this.chat.emit('joinMatch', {
+    this.commandCenter.emit('joinMatch', {
       gameID: gameID
     });
   };
