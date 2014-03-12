999545a17ce46efd9b75a69d88a3cb20819cac17
James Pickard <james.pickard+github@gmail.com>
2014-03-12 12:19:41 -0400

    Add archetypes. Add status server on port 9000.

--- test/player_simulator/simulator.js
+++ test/player_simulator/simulator.js
@@ -1,4 +1,6 @@
-var Player = require('./Player.js');
+var Player = require('./Player.js'),
+  http = require('http'),
+  util = require('util');
 
 function Simulator () {
   this.maxPlayers = 50;
@@ -7,6 +9,20 @@ function Simulator () {
   this.serverAddress = 'http://localhost:3000';
   this.numPlayers = 0;
   this.activePlayers = 0;
+
+  // Player types. Loop through these so we have a good mix.
+  this.archetypes = [
+    'JOINER',
+    'CREATOR'
+  ];
+
+  // Keep track of the players.
+  this.players = [];
+
+  // Create a server for the purpose of showing the statuses.
+  this.server = http.createServer(function (req, res) {
+    res.end(this.playerStatuses());
+  }.bind(this)).listen('9000');
 
   this.addPlayer();
 }
@@ -21,12 +37,14 @@ Simulator.prototype.addPlayer = function() {
   console.log("[Simulator] addPlayer", this.numPlayers);
   this.logStatus();
 
-  new Player({
+  var p = new Player({
     serverAddress: this.serverAddress,
     username: 'simulated' + String(this.numPlayers),
+    archetype: this.archetypes[this.numPlayers % this.archetypes.length],
     manager: this
   });
 
+  this.players.push(p);
   this.numPlayers += 1;
   this.activePlayers += 1;
 
@@ -44,6 +62,19 @@ Simulator.prototype.logStatus = function() {
   console.log("[Simulator] %d total players created, %d active",
     this.numPlayers,
     this.activePlayers);
+};
+
+// Return the statuses of each player.
+Simulator.prototype.playerStatuses = function() {
+  var response = '';
+  for (var i = 0, l = this.players.length; i < l; i += 1) {
+    response += util.format("%s (%s): %s\n",
+      this.players[i].username,
+      this.players[i].archetype,
+      this.players[i].state);
+  }
+
+  return response;
 };
 
 new Simulator();
